6c030a42a85d0d87bfce412d1b96344690351059
Jason Nutt <jnutt367@gmail.com>
2021-05-04 13:58:12 -0400

    commit for earthquakes

--- js/gsap.js
+++ js/gsap.js
@@ -14,9 +14,10 @@ let tl = gsap.timeline({
     }
 })
 
-.fromTo("#blue-bubble", { fill: "red", opacity: 1 }, { repeat: -1, duration: 2, scale: "1.1", ease: "elastic", opacity: 1 })
+.fromTo("#red-bubble", { fill: "red", opacity: 1 }, { repeat: -1, duration: 2, scale: "1.1", ease: "elastic", opacity: 1 })
     .fromTo("#fifth-cloud-up", { x: 0 }, { duration: 3, x: -90, fill: "black", repeat: -1 }, 0)
     .fromTo("#fourth-cloud-up", { x: 0 }, { duration: 1.5, x: -90, fill: "black", repeat: -1 }, 0)
     .fromTo("#bottom-cloud", { x: 0 }, { duration: 2, x: -50, fill: "black", repeat: -1 }, 0)
     .fromTo("#next-cloud-up", { x: 0 }, { duration: 2, x: -100, fill: "black", repeat: -1 }, 0)
     .fromTo("#third-cloud-up", { x: 0 }, { duration: 2, x: -90, fill: "black", repeat: -1 }, 0)
+    .fromTo("#Layer_6", { opacity: 1, x: 0 }, { duration: .1, opacity: 1, x: 5, ease: "bounce.in", repeat: 50 })
